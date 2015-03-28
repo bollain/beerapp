3140ee520c921e28efc6cb0c3b1ba5e36a5d9a8b
riceunit <eric.assaly@gmail.com>
2015-03-28 16:53:24 -0700

    no message

--- Driver.java
+++ Driver.java
@@ -1,5 +1,4 @@
 package model;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,64 +6,107 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 public class Driver {
 
 	public static void main(String[]args)
-	  {
-	    //Initial Greeting
-	    System.out.println("Enter name of beer:");
-	    
-	    //String input by keyboard
-	    java.util.Scanner keyboardReader;
-	    keyboardReader = new java.util.Scanner(System.in);
-	    
-	    //set s1 to be the input
-	    String s1 = keyboardReader.nextLine();
-	    
-	    findBeerID(s1);
-	    
-	    
-	    //Beer input = new Beer(s1);
-	    
-	    //
-	    
-	  }
-
-	private static void findBeerID(String name) {
+	{
 		
+		//Initial Greeting
+		System.out.println("Enter name of beer:");
+		
+		String foodPairing = null;
+		
+		do{
+		if (foodPairing != null){
+			if (foodPairing.equals(""))
+				System.out.println("Not found. Please try again:");
+		}
+		
+		//String input by keyboard
+		java.util.Scanner keyboardReader;
+		keyboardReader = new java.util.Scanner(System.in);
+
+		//set s1 to be the input
+		String s1 = keyboardReader.nextLine();
+
+		foodPairing = findBeerID(s1);
+		
+		System.out.println(foodPairing);
+		
+		}
+		while (foodPairing.equals("") || foodPairing == null);
+		
+	}
+
+	private static String findBeerID(String name) {
+
+		name = name.replaceAll("\\s","%20");
+		name = name.toLowerCase();
+
 		String baseURL = "http://api.brewerydb.com/v2/search?q=";
 
 		String URL = baseURL + name + "&type=beer&format=json&key=cbb8d2ac80296ac3fd5c99451245df9f";
-		
+
+		//System.out.println(URL);
+		String foodPairing = "";
+
 		try {
 			String json = callBreweryDB(URL);
+			//System.out.println(json.substring(0, 20));
 			JSONObject object = new JSONObject(json);
 			
+			JSONArray data = object.getJSONArray("data");
 			
+			JSONObject beer;
+			
+			for (int i = 0; i < data.length(); i++){
+				beer = data.getJSONObject(i);			
+				
+				String jsonString = beer.names().toString();
+
+				if (jsonString.contains("foodPairings")){
+					String foundName = beer.getString("name");
+					System.out.println("We matched your search to: " + foundName + ". Is this correct? (Y/N)");
+					java.util.Scanner keyboardReader;
+					keyboardReader = new java.util.Scanner(System.in);
+					String s1 = keyboardReader.nextLine();
+					if (s1.equals("Y")){
+						foodPairing = beer.getString("foodPairings");
+						break;
+					}
+				}			
+			}
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.getStackTrace();
+			
+		} catch (JSONException e) {
+			e.getStackTrace();
 		}
-		
-		
-		
+
+		return foodPairing;
+
 	}
-	
+
 	private static String callBreweryDB(String httpRequest) throws MalformedURLException, IOException {
 
-        URL url = new URL(httpRequest);
-        HttpURLConnection client = (HttpURLConnection) url.openConnection();
-        InputStream in = client.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String returnString = br.readLine();
-       
-        client.disconnect();
+		URL url = new URL(httpRequest);
+		HttpURLConnection client = (HttpURLConnection) url.openConnection();
+		InputStream in = client.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String returnString = br.readLine();
 
-        return returnString;
-    }
+		client.disconnect();
+
+		return returnString;
+	}
 
 }
